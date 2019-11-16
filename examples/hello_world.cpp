86a90e6f8146f518645efb4fd50d6fb2fde4d6d3
Yunus Ayar <greybley@yahoo.de>
2019-11-17 00:39:28 +0100

    Create hello_world.cpp

---
+++ examples/hello_world.cpp
@@ -0,0 +1,28 @@
+#include <fast_cgi/fast_cgi.hpp>
+
+class responder : public fast_cgi::responder
+{
+public:
+	virtual status_code_type run() override
+	{
+		using namespace fast_cgi::manipulator;
+
+		output() << "Content-type: text/html" << feed << feed;
+		output() << "<html>"
+				 << "<h1>"
+				 << "Hello, World!"
+				 << "</h1>"
+				 << "Your URI: " << params()["REQUEST_URI"] << "</html>";
+	}
+};
+
+int main()
+{
+	// create server
+	fast_cgi::protocol protocol(std::make_shared<tcp_connector>("0.0.0.0", 6545));
+
+	protocol.set_role<responder>();
+
+	protocol.run();
+	protocol.join();
+}
