d9988eda1bc1c2cd3c2fd33ba51af0c0e9c5999c
Yunus Ayar <greybley@yahoo.de>
2019-11-18 16:44:52 +0100

    Update role.hpp

--- fast_cgi/role.hpp
+++ fast_cgi/role.hpp
@@ -11,14 +11,16 @@ namespace fast_cgi {
 class role
 {
 public:
+	typedef detail::quadruple_type status_code_type;
+
 	role() noexcept
 	{
 		_cancelled	 = nullptr;
 		_output_stream = nullptr;
 		_error_stream  = nullptr;
 	}
-	virtual ~role()						 = default;
-	virtual detail::quadruple_type run() = 0;
+	virtual ~role()				   = default;
+	virtual status_code_type run() = 0;
 	volatile bool is_cancelled() const noexcept
 	{
 		return *_cancelled;
