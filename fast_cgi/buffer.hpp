53f49f38e84cfba3a3b332f0dafbc54f52b0a9b0
Yunus Ayar <greybley@yahoo.de>
2019-11-19 21:01:52 +0100

    Add doc to buffer

--- fast_cgi/buffer.hpp
+++ fast_cgi/buffer.hpp
@@ -33,10 +33,14 @@ public:
 		  desired size.
 
 		  @param desired the desired size of the buffer
-		  @returns the buffer pointer and its size; if `size==0` the buffer is full and closed
+		  @returns the buffer pointer and its size; if `size==0` the buffer is full or the token has been closed
 		 */
 		std::pair<void*, std::size_t> request_buffer(std::size_t desired)
 		{
+			if (closed()) {
+				return { nullptr, 0 };
+			}
+
 			// limit buffer to max size
 			desired = std::min(desired, _buffer->_max_size - _buffer->_write_total);
 
@@ -67,15 +71,29 @@ public:
 
 			return { begin, size };
 		}
+		/**
+		  Closes this token. Calling this function on a closed token has no effect.
+		 */
 		void close() noexcept
 		{
-			if (_buffer && _lock.owns_lock()) {
+			if (!closed()) {
 				_lock.unlock();
 				_buffer->_waiter.notify_one();
+
+				_buffer = nullptr;
 			}
+		}
+		/**
+		  Checks whether this token has been closed.
+		 */
+		bool closed() noexcept
+		{
+			return _buffer == nullptr || !_lock.owns_lock();
 		}
 
 	private:
+		friend buffer;
+
 		buffer* _buffer;
 		std::unique_lock<std::mutex> _lock;
 
@@ -85,27 +103,45 @@ public:
 		}
 	};
 
-	buffer(allocator* allocator, std::size_t max_size)
+	/**
+	  Creates an unusable buffer.
+	 */
+	buffer() noexcept
 	{
-		_allocator = allocator;
+		_allocator	 = nullptr;
+		_write_total   = 0;
+		_consume_total = 0;
+		_max_size	  = 0;
+	}
+	/**
+	  Creates a new buffer with the given max size.
 
-		auto page_count = max_size / page::max_size;
-
-		if (max_size % page::max_size) {
-			++page_count;
-		}
-
-		_pages.reserve();
+	  @param allocator the memory allocator
+	  @param max_size the maximum allowed buffer size
+	 */
+	buffer(const std::shared_ptr<allocator>& allocator, std::size_t max_size) : _allocator(allocator)
+	{
+		_write_total   = 0;
+		_consume_total = 0;
+		_max_size	  = max_size;
 	}
 	buffer(const buffer& copy) = delete;
 	buffer(buffer&& move)
 	{}
+	/**
+	  Blocks until the buffer has reached max size.
+	 */
 	void wait_for_all_input()
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
 
 		_waiter.wait(lock, [this] { return _write_total >= _max_size; });
 	}
+	/**
+	  Waits until new input is available. Every input returned by previous calls to this function are invalidated.
+
+	  @returns the new input or `{nullptr, 0}` if no more input is available
+	 */
 	std::pair<void*, std::size_t> wait_for_input()
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
@@ -113,7 +149,7 @@ public:
 
 		// reached end
 		if (_consume_total >= _max_size) {
-			return { nullptr, nullptr };
+			return { nullptr, 0 };
 		}
 
 		// wait for input
@@ -138,10 +174,19 @@ public:
 
 		return { begin, size };
 	}
+	/**
+	  Closes the buffer and prevents any more writes. Calling this function on a closed buffer has no effect.
+	 */
 	void close()
 	{
 		_max_size = _write_total;
 	}
+	/**
+	  Locks the buffer and starts writing. The lock is released when the returned token is destroyed or closed. Sharing
+	  a token between threads results in undefined behavior. This token may **not** exist longer than this instance.
+
+	  @returns the writer token
+	 */
 	writer begin_writing()
 	{
 		return { this };
@@ -157,19 +202,20 @@ private:
 		std::size_t written;
 	};
 
-	allocator* _allocator;
+	std::shared_ptr<allocator> _allocator;
 	std::mutex _mutex;
 	std::condition_variable _waiter;
 	std::deque<page> _pages;
 	std::size_t _write_total;
 	std::size_t _consume_total;
+	/* the maximum allowed size */
 	std::size_t _max_size;
 
 	page append_new_page()
 	{
 		page p{};
 
-		p.begin = _allocator->allocate(0);
+		p.begin = _allocator->allocate(0, 1);
 		p.size  = 0;
 
 		_pages.push_back(p);
