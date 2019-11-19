1659dfa4b4037d4bee257a9c8bad180ae85ff6f2
Yunus Ayar <greybley@yahoo.de>
2019-11-19 15:30:21 +0100

    Update output_manager.hpp

--- fast_cgi/output_manager.hpp
+++ fast_cgi/output_manager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "connection.hpp"
 #include "writer.hpp"
 
 #include <condition_variable>
@@ -17,6 +18,8 @@ class output_manager
 public:
 	typedef std::function<void(writer&)> task_type;
 
+	output_manager(connection& connection) : _writer(connection)
+	{}
 	/**
 	  Adds a writing task to the queue. The task are executed on a different thread at an unspecified time.
 
@@ -32,25 +35,29 @@ public:
 
 		return _queue.back().second.get_future();
 	}
-	void run(writer& writer)
+	void run(volatile bool& alive)
 	{
-		while (true) {
+		while (alive) {
 			std::pair<task_type, std::promise<void>> task;
 
 			// poll queue
 			{
 				std::unique_lock<std::mutex> lock(_mutex);
 
-				_cv.wait(lock, [&] { return !_queue.empty(); });
+				_cv.wait(lock, [&] { return !_queue.empty() || !alive; });
+
+				if (!alive) {
+					break;
+				}
 
 				task = std::move(_queue.front());
 				_queue.pop_front();
 			}
 
 			// execute task
-			task.first(writer);
+			task.first(_writer);
 			task.second.set_value();
-			writer.flush();
+			_writer.flush();
 		}
 	}
 
@@ -58,6 +65,7 @@ private:
 	std::deque<std::pair<task_type, std::promise<void>>> _queue;
 	std::mutex _mutex;
 	std::condition_variable _cv;
+	writer _writer;
 };
 
 } // namespace fast_cgi
