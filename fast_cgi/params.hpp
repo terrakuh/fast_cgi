121d48f16614b6772a74af6c7f914e8c60ae13f9
Yunus Ayar <greybley@yahoo.de>
2019-11-23 20:15:53 +0100

    Update params.hpp

--- fast_cgi/params.hpp
+++ fast_cgi/params.hpp
@@ -12,43 +12,38 @@ namespace fast_cgi {
 class params
 {
 public:
-	constexpr static auto REQUEST_URI	= "REQUEST_URI";
-	constexpr static auto QUERY_STRING   = "QUERY_STRING";
-	constexpr static auto CONTENT_LENGTH = "CONTENT_LENGTH";
-	constexpr static auto CONTENT_TYPE   = "CONTENT_TYPE";
+    constexpr static auto REQUEST_URI    = "REQUEST_URI";
+    constexpr static auto QUERY_STRING   = "QUERY_STRING";
+    constexpr static auto CONTENT_LENGTH = "CONTENT_LENGTH";
+    constexpr static auto CONTENT_TYPE   = "CONTENT_TYPE";
 
-	const std::string& operator[](const std::string& key) const
-	{
-		auto r = parameters.find(key);
-
-		if (r == parameters.end()) {
-		}
-
-		return r->second;
-	}
+    const std::string& operator[](const std::string& key) const
+    {
+        return _parameters.at(key);
+    }
 
 private:
-	friend class request_manager;
+    friend class request_manager;
 
-	std::map<std::string, std::string> parameters;
+    std::map<std::string, std::string> _parameters;
 
-	void _read_parameters(reader& reader)
-	{
-		while (true) {
-			auto pair = detail::name_value_pair::read(reader);
-			std::string name;
+    void _read_parameters(reader& reader)
+    {
+        while (true) {
+            auto pair = detail::name_value_pair::read(reader);
+            std::string name;
 
-			name.resize(header.name_length);
+            name.resize(pair.name_length);
 
-			reader.read(name.data(), header.name_length);
+            reader.read(&name[0], pair.name_length);
 
-			auto& value = parameters[std::move(name)];
+            auto& value = _parameters[std::move(name)];
 
-			value.resize(header.value_length);
+            value.resize(pair.value_length);
 
-			reader.read(value.data(), header.value_length);
-		}
-	}
+            reader.read(&value[0], pair.value_length);
+        }
+    }
 };
 
 } // namespace fast_cgi
