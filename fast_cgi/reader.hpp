d35e1e86903ef5318e74b09e530fc74c0737ded1
Yunus Ayar <greybley@yahoo.de>
2019-11-24 16:13:06 +0100

    Fix SFINAE in reader

--- fast_cgi/reader.hpp
+++ fast_cgi/reader.hpp
@@ -10,58 +10,59 @@ namespace fast_cgi {
 class reader
 {
 public:
-	template<typename T>
-	using valid_type = std::integral_constant<bool, std::is_unsigned<T>::value && (sizeof(T) <= 2 || sizeof(T) == 4)>;
+    template<typename T>
+    using valid_type = std::integral_constant<bool, std::is_unsigned<T>::value && (sizeof(T) <= 2 || sizeof(T) == 4)>;
+    template<typename T>
+    using underlying_type = typename std::enable_if<std::is_enum<T>::value, std::underlying_type<T>>::type;
 
-	virtual ~reader() = default;
-	detail::quadruple_type read_variable()
-	{
-		char buffer[sizeof(detail::quadruple_type)];
-		detail::quadruple_type value = 0;
+    virtual ~reader() = default;
+    detail::quadruple_type read_variable()
+    {
+        char buffer[sizeof(detail::quadruple_type)];
+        detail::quadruple_type value = 0;
 
-		if (read(buffer, 1) != 1) {
-		}
+        if (read(buffer, 1) != 1) {
+        }
 
-		value = buffer[0];
+        value = buffer[0];
 
-		if (value > 127) {
-			// read more
-			if (read(buffer + 1, sizeof(buffer) - 1) != sizeof(buffer) - 1) {
-			}
+        if (value > 127) {
+            // read more
+            if (read(buffer + 1, sizeof(buffer) - 1) != sizeof(buffer) - 1) {
+            }
 
-			value = ((value & 0x7f) << 24) + (buffer[1] << 16) + (buffer[2] << 8) + buffer[3];
-		}
+            value = ((value & 0x7f) << 24) + (buffer[1] << 16) + (buffer[2] << 8) + buffer[3];
+        }
 
-		return value;
-	}
-	template<typename T>
-	typename std::enable_if<(std::is_enum<T>::value && valid_type<typename std::underlying_type<T>::type>::value), T>::type
-		read()
-	{
-		return static_cast<T>(read<typename std::underlying_type<T>::type>());
-	}
-	template<typename T>
-	typename std::enable_if<(!std::is_enum<T>::value && valid_type<T>::value), T>::type read()
-	{
-		char buffer[sizeof(T)];
-		T value = 0;
+        return value;
+    }
+    template<typename T>
+    typename std::enable_if<valid_type<typename underlying_type<T>::type>::value, T>::type read()
+    {
+        return static_cast<T>(read<typename std::underlying_type<T>::type>());
+    }
+    template<typename T>
+    typename std::enable_if<valid_type<T>::value, T>::type read()
+    {
+        char buffer[sizeof(T)];
+        T value = 0;
 
-		// end reached
-		if (read(buffer, sizeof(T)) != sizeof(T)) {
-		}
+        // end reached
+        if (read(buffer, sizeof(T)) != sizeof(T)) {
+        }
 
-		if (sizeof(T) == 1) {
-			value = buffer[0];
-		} else if (sizeof(T) == 2) {
-			value = buffer[0] << 8 + buffer[1];
-		} else {
-			value = ((buffer[0] & 0x7f) << 24) + (buffer[1] << 16) + (buffer[2] << 8) + buffer[3];
-		}
+        if (sizeof(T) == 1) {
+            value = buffer[0];
+        } else if (sizeof(T) == 2) {
+            value = buffer[0] << 8 + buffer[1];
+        } else {
+            value = ((buffer[0] & 0x7f) << 24) + (buffer[1] << 16) + (buffer[2] << 8) + buffer[3];
+        }
 
-		return value;
-	}
-	virtual std::size_t read(void* buffer, std::size_t size) = 0;
-	virtual std::size_t skip(std::size_t size)				 = 0;
+        return value;
+    }
+    virtual std::size_t read(void* buffer, std::size_t size) = 0;
+    virtual std::size_t skip(std::size_t size)               = 0;
 };
 
 } // namespace fast_cgi
