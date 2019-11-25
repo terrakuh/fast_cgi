b3cd5c48cc43e401d0845bea70e150d67de0b68c
Yunus Ayar <greybley@yahoo.de>
2019-11-25 12:14:05 +0100

    Update connection_reader.hpp

--- fast_cgi/connection_reader.hpp
+++ fast_cgi/connection_reader.hpp
@@ -12,40 +12,32 @@ namespace fast_cgi {
 class connection_reader : public reader
 {
 public:
-	connection_reader(const std::shared_ptr<connection>& connection) : connection(connection), buffer(new std::uint8_t[max_size])
-	{
-		cursor = 0;
-		size   = 0;
-	}
-	virtual std::size_t read(void* buffer, std::size_t size) override
-	{
-	}
-	void require_size(std::size_t size)
-	{
-		if (cursor + size > this->size) {
-			this->size -= cursor;
+    connection_reader(const std::shared_ptr<connection>& connection) : _connection(connection)
+    {}
+    virtual std::size_t read(void* buffer, std::size_t size) override
+    {
+        return _connection->read(buffer, size, size);
+    }
+    virtual std::size_t skip(std::size_t size) override
+    {
+        std::int8_t buffer[1024];
 
-			std::memmove(buffer.get(), buffer.get() + cursor, this->size);
+        for (std::size_t i = 0; i < size;) {
+            auto s = std::min(sizeof(buffer), size - i);
+            auto r = _connection->read(buffer, s, s);
 
-			cursor = 0;
+            i += r;
 
-			// read
-			this->size += connection->read(buffer.get() + this->size, size - this->size, max_size - this->size);
-		}
-	}
-	virtual std::size_t skip(std::size_t size) override
-	{
-		while (size > 0) {
-		}
-	}
+            if (s != r) {
+                return i;
+            }
+        }
+
+        return size;
+    }
 
 private:
-	constexpr static auto max_size = 4096;
-
-	std::shared_ptr<connection> connection;
-	std::unique_ptr<std::uint8_t[]> buffer;
-	std::size_t cursor;
-	std::size_t size;
+    std::shared_ptr<connection> _connection;
 };
 
 } // namespace fast_cgi
