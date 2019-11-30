9a18d42a5e7eca16b94651c12c088e18517b25d4
Yunus Ayar <greybley@yahoo.de>
2019-11-30 12:40:20 +0100

    Update connection.hpp

--- fast_cgi/connection.hpp
+++ fast_cgi/connection.hpp
@@ -7,13 +7,12 @@ namespace fast_cgi {
 class connection
 {
 public:
-	typedef std::size_t size_type;
+    typedef std::size_t size_type;
 
-	virtual ~connection()														= default;
-	virtual void close()														= 0;
-	virtual void flush()														= 0;
-	virtual size_type read(void* buffer, size_type at_least, size_type at_most) = 0;
-	virtual size_type write(const void* buffer, size_type size)					= 0;
+    virtual ~connection()                                                       = default;
+    virtual void flush()                                                        = 0;
+    virtual size_type read(void* buffer, size_type at_least, size_type at_most) = 0;
+    virtual size_type write(const void* buffer, size_type size)                 = 0;
 };
 
 } // namespace fast_cgi
