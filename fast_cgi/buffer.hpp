d0cbad906c287b4298bb79f0fcfa21fad06ed6d4
Yunus Ayar <greybley@yahoo.de>
2019-12-05 22:15:48 +0100

    Add input_manager

--- fast_cgi/buffer.hpp
+++ fast_cgi/buffer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "allocator.hpp"
+#include "exception/interrupted_exception.hpp"
 #include "log.hpp"
 
 #include <algorithm>
@@ -112,6 +113,7 @@ public:
      */
     buffer(const std::shared_ptr<allocator>& allocator, std::size_t max_size) : _allocator(allocator)
     {
+        _interrupted   = false;
         _write_total   = 0;
         _consume_total = 0;
         _max_size      = max_size;
@@ -124,19 +126,39 @@ public:
             _allocator->deallocate(page.begin, page.size);
         }
     }
+    void interrupt_all_waiting()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        _interrupted = true;
+        _waiter.notify_all();
+    }
+    bool interrupted()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        return _interrupted;
+    }
     /**
       Blocks until the buffer has reached max size.
+
+      @throws exception::interrupted_exception if reading was interrupted
      */
     void wait_for_all_input()
     {
         std::unique_lock<std::mutex> lock(_mutex);
 
-        _waiter.wait(lock, [this] { return _write_total >= _max_size; });
+        _waiter.wait(lock, [this] { return _write_total >= _max_size || _interrupted; });
+
+        if (_interrupted) {
+            throw exception::interrupted_exception("waiting was interrupted");
+        }
     }
     /**
       Waits until new input is available. Every input returned by previous calls to this function are invalidated.
 
       @returns the new input or `{nullptr, 0}` if no more input is available
+      @throws exception::interrupted_exception if reading was interrupted
      */
     std::pair<void*, std::size_t> wait_for_input()
     {
@@ -152,6 +174,10 @@ public:
 
         // wait for input
         _waiter.wait(lock, [this, &ptr] {
+            if (_interrupted) {
+                return true;
+            }
+
             LOG(TRACE, "page count={}", _pages.size());
 
             for (auto& page : _pages) {
@@ -166,6 +192,11 @@ public:
 
             return false;
         });
+
+        // interrupt only if no input available
+        if (_interrupted && !ptr) {
+            throw exception::interrupted_exception("waiting was interrupted");
+        }
 
         auto begin = static_cast<std::int8_t*>(ptr->begin) + ptr->consumed;
         auto size  = ptr->written - ptr->consumed;
@@ -212,6 +243,7 @@ private:
         std::size_t written;
     };
 
+    bool _interrupted;
     std::shared_ptr<allocator> _allocator;
     std::mutex _mutex;
     std::condition_variable _waiter;
--- fast_cgi/connection.hpp
+++ fast_cgi/connection.hpp
@@ -11,6 +11,7 @@ public:
 
     virtual ~connection()                                                       = default;
     virtual void flush()                                                        = 0;
+    virtual size_type in_available()                                            = 0;
     virtual size_type read(void* buffer, size_type at_least, size_type at_most) = 0;
     virtual size_type write(const void* buffer, size_type size)                 = 0;
 };
